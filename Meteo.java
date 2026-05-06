5aa3b961ea0f7ba78cc7be946c14394621b9283e
Caleb P <prucal01@ecolecatholique.ca>
2026-05-06 15:29:56 -0400

    Create Meteo.java

---
+++ Meteo.java
@@ -0,0 +1,29 @@
+public class Meteo {
+    private int vent;
+    private int temp;
+    private int precip;
+
+    public int getVent() {
+        return vent;
+    }
+
+    public void setVent(int vent) {
+        this.vent = vent;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public int getPrecip() {
+        return precip;
+    }
+
+    public void setPrecip(int precip) {
+        this.precip = precip;
+    }
+}
