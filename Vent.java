be617f687d6a7d412f5533fba53e821254b3e8e7
Caleb P <prucal01@ecolecatholique.ca>
2026-05-07 10:28:44 -0400

    Update Vent.java

--- Vent.java
+++ Vent.java
@@ -1,1 +1,11 @@
+public class Vent extends Meteo {
+    private int vent;
+    
+    public int getVent() {
+        return vent;
+    }
 
+    public void setVent(int vent) {
+        this.vent = vent;
+    }
+}
