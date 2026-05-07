176750690cd664dc6a15d48a82905393113e53a7
Caleb P <prucal01@ecolecatholique.ca>
2026-05-07 10:28:30 -0400

    Update Precip.java

--- Precip.java
+++ Precip.java
@@ -1,1 +1,11 @@
+public class Precip extends Meteo {
+    private int precip;
 
+    public int getPrecip() {
+        return precip;
+    }
+
+    public void setPrecip(int precip) {
+        this.precip = precip;
+    }
+}
