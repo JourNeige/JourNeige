eadfe571f778628604b985176f509761b682ff55
Caleb P <prucal01@ecolecatholique.ca>
2026-05-11 11:53:26 -0400

    Update Periode.java

--- Periode.java
+++ Periode.java
@@ -1,1 +1,11 @@
+public class Periode extends Meteo {
+    private int periodeDebut;
+    private int periodeFin;
 
+    public void setPeriodeDebut(int periodeDebut) {
+        this.periodeDebut = periodeDebut;
+    }
+    public void setPeriodeFin(int periodeFin) {
+        this.periodeFin = periodeFin;
+    }
+}
