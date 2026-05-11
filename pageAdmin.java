c6b81a0698f2d7afd1e7dda3e5cae63bfee46895
Caleb P <prucal01@ecolecatholique.ca>
2026-05-11 11:52:53 -0400

    Update pageAdmin.java

--- pageAdmin.java
+++ pageAdmin.java
@@ -8,27 +8,37 @@ public class pageAdmin extends JFrame implements ActionListener {
     public Precip precip;
     public Temp temp;
     public Vent vent;
+    public Periode periodeDebut;
+    public Periode periodeFin;
+    public Jour jour;
 
     JLabel villeEntrez = new JLabel("Entrez la Ville: ");
     JTextField villeEntrezText = new JTextField(10);
     JLabel postaleEntrez = new JLabel("Entrez la Postale: ");
     JTextField postaleEntrezText = new JTextField(10);
+    JLabel precipEntrez = new JLabel("Entrez la Préciptation: ");
+    JTextField precipEntrezText = new JTextField(10);
+    JLabel periodeDebutEntrez = new JLabel("Entrez le Début du neige: ");
+    JTextField periodeDebutEntrezText = new JTextField(10);
+    JLabel periodeFinEntrez = new JLabel("Entrez la Fin du neige: ");
+    JTextField periodeFinEntrezText = new JTextField(10);
     JLabel tempEntrez = new JLabel("Entrez la Température: ");
     JTextField tempEntrezText = new JTextField(10);
     JLabel ventEntrez = new JLabel("Entrez la Vitesse du Vent: ");
     JTextField ventEntrezText = new JTextField(10);
-    JLabel precipEntrez = new JLabel("Entrez la Préciptation: ");
-    JTextField precipEntrezText = new JTextField(10);
     JButton boutonEntrez = new JButton("Entrez");
-    GridLayout leGrid = new GridLayout(6, 1);
+    GridLayout leGrid = new GridLayout(9, 1);
     FlowLayout leFlow = new FlowLayout();
     JButton boutonRetour = new JButton("Retour");
 
-    public pageAdmin(Precip precip, Temp temp, Vent vent) {
+    public pageAdmin(Precip precip, Temp temp, Vent vent, Periode periodeDebut, Periode periodeFin, Jour jour) {
         super("Page Admin");
         this.precip = precip;
         this.temp = temp;
         this.vent = vent;
+        this.periodeDebut = periodeDebut;
+        this.periodeFin = periodeFin;
+        this.jour = jour;
 
 
         setLayout(leGrid);
@@ -59,44 +69,65 @@ public class pageAdmin extends JFrame implements ActionListener {
 
         JPanel rangee4 = new JPanel();
         rangee4.setLayout(leFlow);
-        rangee4.add(tempEntrez);
-        rangee4.add(tempEntrezText);
+        rangee4.add(periodeDebutEntrez);
+        rangee4.add(periodeDebutEntrezText);
         add(rangee4);
 
         JPanel rangee5 = new JPanel();
         rangee5.setLayout(leFlow);
-        rangee5.add(ventEntrez);
-        rangee5.add(ventEntrezText);
+        rangee5.add(periodeFinEntrez);
+        rangee5.add(periodeFinEntrezText);
         add(rangee5);
 
         JPanel rangee6 = new JPanel();
         rangee6.setLayout(leFlow);
-        rangee6.add(boutonEntrez);
-        boutonEntrez.addActionListener(this);
-        rangee6.add(boutonRetour);
-        boutonRetour.addActionListener(this);
+        rangee6.add(tempEntrez);
+        rangee6.add(tempEntrezText);
         add(rangee6);
-    }
 
+        JPanel rangee7 = new JPanel();
+        rangee7.setLayout(leFlow);
+        rangee7.add(ventEntrez);
+        rangee7.add(ventEntrezText);
+        add(rangee7);
+
+        JPanel rangee8 = new JPanel();
+        rangee8.setLayout(leFlow);
+        rangee8.add(boutonEntrez);
+        boutonEntrez.addActionListener(this);
+        rangee8.add(boutonRetour);
+        boutonRetour.addActionListener(this);
+        add(rangee8);
+
+        JPanel rangee9 = new JPanel();
+        rangee9.setLayout(leFlow);
+        rangee9.add(boutonEntrez);
+        boutonEntrez.addActionListener(this);
+        rangee9.add(boutonRetour);
+        boutonRetour.addActionListener(this);
+        add(rangee9);
+    }
     public void actionPerformed(ActionEvent actionEvent) {
-        int precip1 = 0;
-        int temp1 = 0;
-        int vent1 = 0;
-        String ville1 = "";
 
         if (actionEvent.getSource() == boutonEntrez) {
-            ville1 = villeEntrezText.getText();
+            int jour1 = Integer.parseInt(JOptionPane.showInputDialog(this, "Entrez le Jour (1-31):"));
+            String ville1 = villeEntrezText.getText();
             String postale1 = postaleEntrezText.getText();
-            precip1 = Integer.parseInt(precipEntrezText.getText());
-            temp1 = Integer.parseInt(tempEntrezText.getText());
-            vent1 = Integer.parseInt(ventEntrezText.getText());
+            int precip1 = Integer.parseInt(precipEntrezText.getText());
+            int temp1 = Integer.parseInt(tempEntrezText.getText());
+            int vent1 = Integer.parseInt(ventEntrezText.getText());
+            int periode1 = (Integer.parseInt(periodeDebutEntrezText.getText()));
+            int periode2 = (Integer.parseInt(periodeFinEntrezText.getText()));
             precip.setPrecip(precip1);
+            periodeDebut.setPeriodeDebut(periode1);
+            periodeFin.setPeriodeFin(periode2);
             temp.setTemp(temp1);
             vent.setVent(vent1);
-            JOptionPane.showMessageDialog(this, "Données entrées pour " + ville1 + " (" + postale1 + "):\nPrécipitation: " + precip1 + "\nTempérature: " + temp1 + "\nVitesse du Vent: " + vent1);
+            jour.setJour(jour1);
+            JOptionPane.showMessageDialog(this, "Données entrées pour du " + jour1 + "e jour\n" + ville1 + " (" + postale1 + "):\nPrécipitation: " + precip1 + ", Début du Période: " + periode1 + ", Fin du Période: " + periode2 + "\nTempérature: " + temp1 + "\nVitesse du Vent: " + vent1);
                 try {
                     FileWriter myWriter = new FileWriter("Donnees.txt", true);
-                    myWriter.write("Ville: " + ville1 + ", Postale: " + postale1 + ", Précipitation: " + precip1 + ", Température: " + temp1 + ", Vitesse du Vent: " + vent1 + "\n");
+                    myWriter.write("Ville: " + ville1 + ", Postale: " + postale1 + ", Précipitation: " + precip1 + ", Début du Période: " + periode1 + ", Fin du Période: " + periode2 + ", Température: " + temp1 + ", Vitesse du Vent: " + vent1 + "\n");
                     myWriter.close();
                 } catch (IOException e) {
                     JOptionPane.showMessageDialog(this, "Aucun fichier de données trouvé.");
@@ -104,7 +135,7 @@ public class pageAdmin extends JFrame implements ActionListener {
                 }
         }
         if (actionEvent.getSource() == boutonRetour) {
-            new Debut(precip, temp, vent);
+            new Debut(precip, temp, vent, periodeDebut, periodeFin, jour);
             dispose();
         }
     }
@@ -113,6 +144,9 @@ public class pageAdmin extends JFrame implements ActionListener {
             Precip precip = new Precip();
             Temp temp = new Temp();
             Vent vent = new Vent();
-            new pageAdmin(precip, temp, vent);
+            Periode periodeDebut = new Periode();
+            Periode periodeFin = new Periode();
+            Jour jour = new Jour();
+            new pageAdmin(precip, temp, vent, periodeDebut, periodeFin, jour);
         }
     }
