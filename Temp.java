1153a1e44a7b701fc3c908a4649e4ee9624fed89
Caleb P <prucal01@ecolecatholique.ca>
2026-05-11 11:54:10 -0400

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -1,10 +1,6 @@
 public class Temp extends Meteo {
     private int temp;
-
-     public int getTemp() {
-        return temp;
-    }
-
+    
     public void setTemp(int temp) {
         this.temp = temp;
     }
