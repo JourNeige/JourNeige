ca436a84eb16fbbd75a57fe20d981ab6366fee00
Caleb P <prucal01@ecolecatholique.ca>
2026-05-12 15:28:03 -0400

    Update Jour.java

--- Jour.java
+++ Jour.java
@@ -1,7 +1,11 @@
 public class Jour extends Meteo {
-    private int jour;
-
-    public void setJour(int jour) {
-        this.jour = jour;
+    private int jourDebut;
+    private int jourFin;
+    
+    public void setJourDebut(int jourDebut) {
+        this.jourDebut = jourDebut;
+    }
+    public void setJourFin(int jourFin) {
+        this.jourFin = jourFin;
     }
 }
