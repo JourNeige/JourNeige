66b99432b03e693ed4c8244b02abe7971898303d
Caleb P <prucal01@ecolecatholique.ca>
2026-05-20 10:32:14 -0400

    Update Meteo.java

--- Meteo.java
+++ Meteo.java
@@ -2,34 +2,22 @@ public class Meteo {
     int precip;
     int temp;
     int vent;
-    int periode;
-    int score;
-    int jourDebut;
-    int jourFin;
-
+    
     public int getVent() {
         return vent;
     }
+
      public int getTemp() {
         return temp;
     }
+
     public int getPrecip() {
         return precip;
     }
 
-    public int getPeriode() {
-        return periode;
-    }
-
-    public int getJourDebut() {
-        return jourDebut;
-    }
-
-    public int getJourFin() {
-        return jourFin;
-    }
-
-    public int getScore() {
-        return score;
+    public void setMeteo(int precip1, int temp1, int vent1) {
+        this.vent = vent;
+        this.temp = temp;
+        this.precip = precip;
     }
 }
